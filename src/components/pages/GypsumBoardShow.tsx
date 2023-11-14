1eec960e8396faab8f5e65a4052c7daebaf3d4f0
Artem Maklashev <artem@maklashev.ru>
2023-11-14 12:01:04 +0400

    ввод месяца и даты для показа таблицы

--- src/components/pages/GypsumBoardShow.tsx
+++ src/components/pages/GypsumBoardShow.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import GypsumBoardInputData from "../../model/inputData/GypsumBoardInputData";
 import GypsumBoardTable from "./GypsumBoardTable";
 
@@ -11,83 +11,72 @@ const GypsumBoardShow: React.FC<GypsumBoardShowProps> = (props) => {
     const [monthIndex, setMonthIndex] = useState<number>(4); // начальное значение месяца
     const [year, setYear] = useState<number>(2023); // начальное значение года
 
-    const fetchGypsumBoardData = useCallback(async () => {
-        try {
-            setIsLoading(true);
-            const params = new URLSearchParams({
-                month: monthIndex.toString(),
-                year: year.toString(),
-            });
+    useEffect(() => {
+        const fetchGypsumBoardData = async () => {
+            try {
+                setIsLoading(true);
+                const params = new URLSearchParams({
+                    month: monthIndex.toString(),
+                    year: year.toString(),
+                });
 
-            const response = await fetch(`http://localhost:8080/api/allboard?${params.toString()}`);
+                const response = await fetch(`http://localhost:8080/api/allboard?${params.toString()}`);
 
-            if (!response.ok) {
-                throw new Error(`Ошибка при запросе: ${response.status} ${response.statusText}`);
+                if (!response.ok) {
+                    throw new Error(`Ошибка при запросе: ${response.status} ${response.statusText}`);
+                }
+
+                const data: GypsumBoardInputData[] = await response.json();
+                setErrorText(null);
+                setGypsumBoardData(data);
+            } catch (error: any) {
+                console.error(`Произошла ошибка: ${error.message}`);
+                setErrorText(error.message);
+                setGypsumBoardData([]);
+            } finally {
+                setIsLoading(false);
             }
+        };
 
-            const data: GypsumBoardInputData[] = await response.json();
-            setErrorText(null);
-            setGypsumBoardData(data);
-        } catch (error: any) {
-            console.error(`Произошла ошибка: ${error.message}`);
-            setErrorText(error.message);
-            setGypsumBoardData([]);
-        } finally {
-            setIsLoading(false);
-        }
+        fetchGypsumBoardData();
     }, [monthIndex, year]);
 
-    useEffect(() => {
-        fetchGypsumBoardData();
-    }, [fetchGypsumBoardData]);
-    const calculateTotal = <K extends keyof GypsumBoardInputData>(data: GypsumBoardInputData[], property: K): number => {
-        return data.reduce((total, item) => total + Number(item[property]), 0);
-    };
-
-    const calculatePercentageTotal = (data: GypsumBoardInputData[]): string => {
-        const totalFact = calculateTotal(data, 'factValue');
-        const totalTotal = calculateTotal(data, 'total');
-        return totalTotal > 0 ? (((totalTotal - totalFact) * 100) / totalTotal).toFixed(2) + "%" : "---";
-    };
-
     const handleMonthChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setMonthIndex(parseInt(event.target.value, 10));
-        fetchGypsumBoardData();
+        const newMonthIndex = parseInt(event.target.value.substring(5), 10);
+        console.log('Month changed to:', newMonthIndex);
+        setMonthIndex(newMonthIndex);
     };
 
     const handleYearChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setYear(parseInt(event.target.value, 10));
-        fetchGypsumBoardData();
+        const newYear = parseInt(event.target.value, 10);
+        console.log('Year changed to:', newYear);
+        setYear(newYear);
     };
-
-    // ... остальной код остается без изменений
 
     return (
         <div className="container">
             <div>
                 <div className="input-group mb-3">
                     <span className="input-group-text" id="basic-addon1">Месяц</span>
-                {/*<label htmlFor="monthInput">Месяц:</label>*/}
-                <input
-                    type="month"
-                    id="monthInput"
-                    value={`${year}-${monthIndex.toString().padStart(2, '0')}`}
-                    onChange={handleMonthChange}
-                />
+                    <input
+                        type="month"
+                        id="monthInput"
+                        value={`${year}-${(monthIndex).toString().padStart(2, '0')}`}
+                        onChange={handleMonthChange}
+                    />
                 </div>
             </div>
             <div>
                 <div className="input-group mb-3">
                     <span className="input-group-text" id="basic-addon2">Год</span>
-                <input
-                    type="number"
-                    id="yearInput"
-                    value={year}
-                    onChange={handleYearChange}
-                />
+                    <input
+                        type="number"
+                        id="yearInput"
+                        value={year}
+                        onChange={handleYearChange}
+                    />
                 </div>
             </div>
-            {/* Вместо прорисовки таблицы здесь используем GypsumBoardTable */}
             <GypsumBoardTable data={gypsumBoardData} />
         </div>
     );
