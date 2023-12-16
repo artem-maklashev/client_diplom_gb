e0de8c1bebd5b0df4d3a0afe0086594e56f78e86
Маклашев Артем <maklashev_ak@outlook.com>
2023-12-16 13:24:46 +0400

    вынесено получение productionData в GypsumBoardShow2.tsx в отдельный файл GetProductionData.ts

--- src/components/pages/GypsumBoardShow2.tsx
+++ src/components/pages/GypsumBoardShow2.tsx
@@ -8,6 +8,7 @@ import BoardProduction from "../../model/production/BoardProduction";
 import EdgeChart from "./gypsumBoardElements/EdgeChart";
 import DefectChart from "./gypsumBoardElements/DefectChart";
 import ThicknessChart from "./gypsumBoardElements/ThicknessChart";
+import {useFetchProductionData} from "./commonElements/GetProductionData";
 
 interface GypsumBoardShowProps {
 }
@@ -17,8 +18,8 @@ const GypsumBoardShow: React.FC<GypsumBoardShowProps> = () => {
     const [errorText, setErrorText] = useState<string | null>(null);
     const [selectedStartDate, setSelectedStartDate] = useState<string>(getFirstDate()); // Set initial date to today
     const [selectedEndDate, setSelectedEndDate] = useState<string>(getCurrentDate()); // Set initial date to today
-    const [productionData, setProductionData] = useState<BoardProduction[]>([]);
-
+    // const [productionData, setProductionData] = useState<BoardProduction[]>([]);
+    const {productionData, } = useFetchProductionData(selectedStartDate, selectedEndDate);
     const fetchGypsumBoardData = useCallback(async () => {
         try {
 
@@ -42,30 +43,30 @@ const GypsumBoardShow: React.FC<GypsumBoardShowProps> = () => {
             setGypsumBoardData([]);
         }
     }, [selectedStartDate, selectedEndDate]);
-    const fetchProductionData = useCallback(async () => {
-        try {
-            const params = new URLSearchParams({
-                startDate: selectedStartDate,
-                endDate: selectedEndDate
-            });
-
-            const response = await fetch(`http://localhost:8080/api/allboard/production?${params.toString()}`);
-
-            if (!response.ok) {
-                throw new Error(`Ошибка при запросе: ${response.status} ${response.statusText}`);
-            }
-
-            const data: BoardProduction[] = await response.json();
-            setErrorText(null);
-            console.log(data[0].productionList.id);
-            setProductionData(data);
-            console.log("Получены данные по выпуску продукции " + data.length);
-        } catch (error: any) {
-            console.error(`Произошла ошибка: ${error.message}`);
-            setErrorText(error.message);
-            setProductionData([]);
-        }
-    }, [selectedStartDate, selectedEndDate]);
+    // const fetchProductionData = useCallback(async () => {
+    //     try {
+    //         const params = new URLSearchParams({
+    //             startDate: selectedStartDate,
+    //             endDate: selectedEndDate
+    //         });
+    //
+    //         const response = await fetch(`http://localhost:8080/api/allboard/production?${params.toString()}`);
+    //
+    //         if (!response.ok) {
+    //             throw new Error(`Ошибка при запросе: ${response.status} ${response.statusText}`);
+    //         }
+    //
+    //         const data: BoardProduction[] = await response.json();
+    //         setErrorText(null);
+    //         console.log(data[0].productionList.id);
+    //         setProductionData(data);
+    //         console.log("Получены данные по выпуску продукции " + data.length);
+    //     } catch (error: any) {
+    //         console.error(`Произошла ошибка: ${error.message}`);
+    //         setErrorText(error.message);
+    //         setProductionData([]);
+    //     }
+    // }, [selectedStartDate, selectedEndDate]);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -75,13 +76,13 @@ const GypsumBoardShow: React.FC<GypsumBoardShowProps> = () => {
         fetchData();
     }, [selectedStartDate, selectedEndDate, fetchGypsumBoardData]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            await fetchProductionData();
-        };
-
-        fetchData();
-    }, [selectedStartDate, selectedEndDate, fetchProductionData]);
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         await fetchProductionData();
+    //     };
+    //
+    //     fetchData();
+    // }, [selectedStartDate, selectedEndDate, fetchProductionData]);
 
 
     const handleDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
---
+++ src/components/pages/commonElements/GetProductionData.ts
@@ -0,0 +1,45 @@
+import { useEffect, useState, useCallback } from 'react';
+import BoardProduction from '../../../model/production/BoardProduction';
+
+
+export const useFetchProductionData = (
+    selectedStartDate: string,
+    selectedEndDate: string
+) => {
+    const [productionData, setProductionData] = useState<BoardProduction[]>([]);
+    const [errorText, setErrorText] = useState<string | null>(null);
+
+    const fetchProductionData = useCallback(async () => {
+        try {
+            const params = new URLSearchParams({
+                startDate: selectedStartDate,
+                endDate: selectedEndDate,
+            });
+
+            const response = await fetch(`http://localhost:8080/api/allboard/production?${params.toString()}`);
+
+            if (!response.ok) {
+                throw new Error(`Ошибка при запросе: ${response.status} ${response.statusText}`);
+            }
+
+            const data: BoardProduction[] = await response.json();
+            setErrorText(null);
+            setProductionData(data);
+            console.log("Получены данные по выпуску продукции " + data.length);
+        } catch (error: any) {
+            console.error(`Произошла ошибка: ${error.message}`);
+            setErrorText(error.message);
+            setProductionData([]);
+        }
+    }, [selectedStartDate, selectedEndDate]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            await fetchProductionData();
+        };
+
+        fetchData();
+    }, [selectedStartDate, selectedEndDate, fetchProductionData]);
+
+    return { productionData, errorText };
+};
