57d764dd83467f9937963787eee9a45259308b08
Maklashev-Artem <artem@maklashev.ru>
2023-12-26 21:04:49 +0400

    ghjvt;enjxysq

--- src/components/navigation/Footer.tsx
+++ src/components/navigation/Footer.tsx
@@ -2,7 +2,7 @@ import { Navbar } from "react-bootstrap";
 
 const Footer = () => {
     return (
-        <Navbar fixed="bottom" bg="dark" variant="dark">
+        <Navbar expand="lg" fixed="bottom" bg="dark" variant="dark">
             <Navbar.Brand>&copy; 2023 Маклашев Артем</Navbar.Brand>
         </Navbar>
     );
