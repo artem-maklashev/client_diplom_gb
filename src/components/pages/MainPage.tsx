841bc57471a48c7d28b732723377b1c496d57d93
Маклашев Артем <maklashev_ak@outlook.com>
2024-01-10 23:30:36 +0400

    добавлен класс ApiService.ts На основную страницу добавлены показатели производства

--- src/components/pages/MainPage.tsx
+++ src/components/pages/MainPage.tsx
@@ -1,6 +1,9 @@
 import React, {useCallback, useState} from "react";
 import Plan from "../../model/gypsumBoard/Plan";
-import {Col, Container, Row} from "react-bootstrap";
+import {Card, Col, Container, Row} from "react-bootstrap";
+import ApiService from "../../service/ApiService";
+import BoardProduction from "../../model/production/BoardProduction";
+import {forEach} from "react-bootstrap/ElementChildren";
 
 interface MainPageProps {
 }
@@ -8,59 +11,100 @@ interface MainPageProps {
 const MainPage: React.FC<MainPageProps> = () => {
     const [boardPlanData, setBoardPlanData] = React.useState<Plan[]>([]);
     const [errorText, setErrorText] = useState<string | null>(null);
-    const planDate = () => {
-        const now = new Date();
-        const year = now.getUTCFullYear();
-        const month = (now.getUTCMonth() + 1).toString().padStart(2, '0'); // Months are zero-based
-        const day = now.getUTCDate().toString().padStart(2, '0');
-        return `${year}-${month}-${day}`;
-    }
+    const [boardProductionData, setBoardProductionData] = React.useState<BoardProduction[]>([]);
+
     const fetchPlan = useCallback(async () => {
         try {
-            // const params = new URLSearchParams(
-            //     {planDate: planDate()}
-            // );
-
-            // const response = await fetch(`${process.env.REACT_APP_API_URL}/planData?${params.toString()}`);
-            const response = await fetch(`${process.env.REACT_APP_API_URL}/planData`);
-            if (!response.ok) {
-                throw new Error(`Ошибка при запросе: ${response.status} ${response.statusText}`);
-            }
-
-            const data: Plan[] = await response.json();
+            const data = await ApiService.fetchTodayPlan();
             setErrorText(null);
             setBoardPlanData(data);
         } catch (error: any) {
-            console.error(`Произошла ошибка: ${error.message}`);
             setErrorText(error.message);
             setBoardPlanData([]);
+        }
+    }, []);
+
+    const fetchBoardProduction = useCallback(async () => {
+        try {
+            const data = await ApiService.fetchTodayBoardProduction();
+            setErrorText(null);
+            setBoardProductionData(data);
+        } catch (error: any) {
+            setErrorText(error.message);
+            setBoardProductionData([]);
         }
     }, []);
 
     React.useEffect(() => {
         const fetchData = async () => {
             await fetchPlan();
+            await fetchBoardProduction();
         };
 
         fetchData();
-    }, [fetchPlan]);
-    // alert("Получены данные по плану в размере " + boardPlanData.length);
+    }, [fetchPlan, fetchBoardProduction]);
+
     const plan = boardPlanData.reduce((acc, plan) => acc + (plan.planValue), 0);
+    const todayPlan = boardPlanData.filter(plan => plan.plan_date === new Date());
+    const sortedBoardProduction = boardProductionData.filter(board => board.gypsumBoardCategory.id < 5);
+    const { total, value, defectPercent } = sortedBoardProduction.reduce(
+        (acc, board) => {
+            const isCategory1 = board.gypsumBoardCategory.id === 1;
+            acc.total += isCategory1 ? board.value : 0;
+            acc.value += isCategory1 ? 0 : board.value;
+            return acc;
+        },
+        { total: 0, value: 0, defectPercent: 0 }
+    );
 
-
-
+    const defectPercentResult = total === 0 ? 0 : ((total - value) / total) * 100;
 
     return (
-        <Row>
-            <Container>
-                <Row className="mt-5">
-                    <Col className="mt-3">
-                        <h3>План на текущий месяц</h3>
-                        <p> {plan}</p>
+
+                <Row className="mt-5 justify-content-center text-center" >
+                <p></p>
+                    <h2>Показатели за текущий месяц</h2>
+                    <Col className="mt-3 col-lg-3 col-sm-12">
+                        <Card className="text-center bg-body-secondary ">
+                            <Card.Body>
+                                <Card.Header className="mb-2"><h3>Производство ГСП</h3></Card.Header>
+                                <Card.Subtitle>План на текущий месяц</Card.Subtitle>
+                                <Card.Text>
+                                    <p> {plan}</p>
+                                </Card.Text>
+                                <Card.Subtitle>Процент брака</Card.Subtitle>
+                                <Card.Text>
+                                    {defectPercentResult.toFixed(2)}%
+                                </Card.Text>
+                                <Card.Subtitle>Сегодня в производстве</Card.Subtitle>
+                                {todayPlan.length > 0 ? (
+                                    <table className="table">
+                                        <thead>
+                                        <tr>
+                                            <th>Гипсокартон</th>
+                                            <th>Количество</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        {todayPlan.map(board => (
+                                            <tr key={board.gypsum_board_id.toString()}>
+                                                <td>{board.gypsum_board_id.toString()}</td>
+                                                <td>{board.planValue}</td>
+                                            </tr>
+                                        ))}
+                                        </tbody>
+                                    </table>
+                                ) : (
+                                    <p>На сегодня производство не запланировано.</p>
+                                )}
+                            </Card.Body>
+
+
+                        </Card>
                     </Col>
                 </Row>
-            </Container>
-        </Row>
+
+
     );
 }
 export default MainPage;
---
+++ src/service/ApiService.ts
@@ -0,0 +1,50 @@
+import Plan from "../model/gypsumBoard/Plan";
+import BoardProduction from "../model/production/BoardProduction";
+
+class ApiService {
+    private static baseUrl = process.env.REACT_APP_API_URL;
+
+    static async fetchTodayPlan(): Promise<Plan[]> {
+        try {
+            const response = await fetch(`${process.env.REACT_APP_API_URL}/planData`);
+            if (!response.ok) {
+                throw new Error(`Ошибка при запросе: ${response.status} ${response.statusText}`);
+            }
+            return await response.json();
+        } catch (error: any) {
+            console.error(`Произошла ошибка: ${error.message}`);
+            throw error;
+        }
+    }
+
+    static async fetchTodayBoardProduction(): Promise<BoardProduction[]> {
+
+        try {
+            const now = new Date();
+            const params = new URLSearchParams({
+
+                    // startDate: this.getFormattedDate(new Date(now.getFullYear(), now.getMonth(), 1)),
+                    // endDate: this.getFormattedDate(now)
+                    startDate: this.getFormattedDate(new Date(2023, 12, 1)),
+                    endDate: this.getFormattedDate(new Date(2023,12, 10))
+                })
+            ;
+
+            const response = await fetch(`${process.env.REACT_APP_API_URL}/allboard/production?${params.toString()}`);
+            return await response.json();
+        } catch (error: any) {
+            console.error(`Произошла ошибка: ${error.message}`);
+            throw error;
+        }
+    }
+
+    static getFormattedDate(date: Date): string {
+        const year = date.getUTCFullYear();
+        const month = (date.getUTCMonth() + 1).toString().padStart(2, '0');
+        const day = date.getUTCDate().toString().padStart(2, '0');
+
+        return `${year}-${month}-${day}`;
+    }
+}
+
+export default ApiService;
